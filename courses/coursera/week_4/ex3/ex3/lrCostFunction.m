5db37a65aa835d44e7f63fcc8fa78b8cc431fd26
Marcos Santamaria <marcosc74@gmail.com>
2018-04-26 08:11:35 +0200

    Working on cost function week_4

--- courses/coursera/week_4/ex3/ex3/lrCostFunction.m
+++ courses/coursera/week_4/ex3/ex3/lrCostFunction.m
@@ -36,12 +36,21 @@ grad = zeros(size(theta));
 %           grad = grad + YOUR_CODE_HERE (using the temp variable)
 %
 
+disp(size(theta));
+disp(size(X));
 
+h=sigmoid(X*theta);
+disp(h);
 
+disp("-----------");
+disp(size(y));
 
+J=(1/m)*(-y' * log(h) - (1 - y)'*log( 1-h ) );
+disp(size(J));
+disp(J);
 
-
-
+grad=(1/m) .* ( (h-y)' * X );
+disp(grad);
 
 
 
